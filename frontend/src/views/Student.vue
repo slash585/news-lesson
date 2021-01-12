fef82d779f167e97fbc55b5f3a3e87dbf92d9214
slash585 <51036040+slash585@users.noreply.github.com>
2021-01-12 04:08:07 +0300

    Update Student.vue
    Write student generated news

--- frontend/src/views/Student.vue
+++ frontend/src/views/Student.vue
@@ -25,18 +25,48 @@ export default {
 <template lang="pug">
 div
   h1 Student: {{student.name}}
-  .questions(v-for="q in questions")
-    .questions(v-for="question in q.questions")
-     h6 Who: {{question.who}}
-     h6 Where: {{question.where}}
-     h6 When: {{question.when}}
-     h6 Why: {{question.why}}
-     h6 What: {{question.what}}
-     h6 How: {{question.how}}
-     a(:href="`/students/${student._id}/createNews/${q._id}`") Create News
-     br
-     hr
-    
-   
-
+  .questions
+    .question(v-for="q in questions")
+     div(v-for="question in q.questions")
+      h6 Who: {{question.who}}
+      h6 Where: {{question.where}}
+      h6 When: {{question.when}}
+      h6 Why: {{question.why}}
+      h6 What: {{question.what}}
+      h6 How: {{question.how}}
+      a(:href="`/students/${student._id}/createNews/${q._id}`") Create News
+  .news
+    .n(v-for="news in student.news")
+      p {{news.newsText}}
+      
+      
+        
 </template>
+
+<style scoped>
+.questions{
+  display: flex;
+  flex-direction: row;
+}
+
+.question{
+  flex: 0.33;
+  border: 1px solid black;
+  border-radius: 5px;
+  margin: 3px;
+  color: red;
+}
+
+.news{
+  display: flex;
+  flex-direction: column;
+}
+
+.n{
+  flex: 0.33;
+  border: 1px solid black;
+  border-radius: 5px;
+  margin: 25px 3px 3px 3px;
+  color: red;
+}
+</style>
